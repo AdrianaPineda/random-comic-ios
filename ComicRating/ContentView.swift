4b4a95162f492111a9c3a377f295177653634ac1
Adriana Pineda <apineda@twilio.com>
2020-02-14 06:44:23 +0200

    First commit

---
+++ ComicRating/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComicRating
+//
+//  Created by Adriana Pineda on 11/11/2019.
+//  Copyright © 2019 Adriana Pineda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
