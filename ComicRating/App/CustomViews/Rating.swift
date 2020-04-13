b83cd9cfbf4bb849236b7e558351de97d9704cb5
Adriana Pineda <apineda@twilio.com>
2020-04-13 08:12:21 +0300

    Configure rating view

--- ComicRating/App/CustomViews/Rating.swift
+++ ComicRating/App/CustomViews/Rating.swift
@@ -8,17 +8,64 @@
 
 import UIKit
 
-class Rating: UIView {
+enum StarRating {
+    case One
+    case Two
+    case Three
+    case Four
+    case Five
 
+    var value: Int {
+        switch self {
+            case .One:
+                return 1
+            case .Two:
+                return 2
+            case .Three:
+                return 3
+            case .Four:
+                return 4
+            default:
+                return 5
+        }
+    }
+
+    static func getStarRating(identifier: String) -> StarRating {
+        switch identifier {
+            case "star_one":
+                return StarRating.One
+            case "star_two":
+                return StarRating.Two
+            case "star_three":
+                return StarRating.Three
+            case "star_four":
+                return StarRating.Four
+            default:
+                return StarRating.Five
+        }
+    }
+}
+
+let filledImageStarName = "filled_star"
+let emptiedImageStarName = "emptied_star"
+
+class Rating: UIView {
     @IBOutlet var container: UIView!
+
+    @IBOutlet var ratingOne: UIImageView!
+    @IBOutlet var ratingTwo: UIImageView!
+    @IBOutlet var ratingThree: UIImageView!
+    @IBOutlet var ratingFour: UIImageView!
+    @IBOutlet var ratingFive: UIImageView!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
+        self.setupView()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setupView()
+        self.setupView()
     }
 
     private func setupView() {
@@ -26,5 +73,23 @@ class Rating: UIView {
         addSubview(self.container)
         self.container.frame = self.bounds
         self.container.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+    // MARK: - Clicked Star
+
+    @IBAction func didClickStar(_ gesture: UITapGestureRecognizer) {
+        guard let identifier = gesture.view?.accessibilityIdentifier else {
+            return
+        }
+        let rating = StarRating.getStarRating(identifier: identifier)
+        self.fillStarsWithRating(rating: rating.value)
+    }
+
+    func fillStarsWithRating(rating: Int) {
+        self.ratingOne.image = UIImage(named: rating >= StarRating.One.value ? filledImageStarName : emptiedImageStarName)
+        self.ratingTwo.image = UIImage(named: rating >= StarRating.Two.value ? filledImageStarName : emptiedImageStarName)
+        self.ratingThree.image = UIImage(named: rating >= StarRating.Three.value ? filledImageStarName : emptiedImageStarName)
+        self.ratingFour.image = UIImage(named: rating >= StarRating.Four.value ? filledImageStarName : emptiedImageStarName)
+        self.ratingFive.image = UIImage(named: rating >= StarRating.Five.value ? filledImageStarName : emptiedImageStarName)
     }
 }
