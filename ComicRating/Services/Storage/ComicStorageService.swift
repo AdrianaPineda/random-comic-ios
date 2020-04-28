86537add6ef6aad0769581dff1a9f3b84b0392e2
Adriana Pineda <apineda@twilio.com>
2020-04-28 07:32:24 +0300

    Refactor storage as it is not needed anymore

--- ComicRating/Services/Storage/ComicStorageService.swift
+++ ComicRating/Services/Storage/ComicStorageService.swift
@@ -41,41 +41,21 @@ class ComicStorageService: ComicStorageServiceInterface {
         }
     }
 
-    private func getManagedObject(id: Int, managedContext: NSManagedObjectContext) -> NSManagedObject? {
-        let predicate = NSPredicate(format: "id == \(id)")
-        let comicsManagedObjects = self.fetchComicRating(withPredicate: predicate, managedContext: managedContext) // send context?
-
-        if comicsManagedObjects.isEmpty {
-            guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: managedContext) else {
-                return nil
-            }
-
-            return NSManagedObject(entity: entity, insertInto: managedContext)
-        }
-        return comicsManagedObjects[0]
-    }
-
     func fetchComicRating() -> [ComicRating] {
         guard let managedContext = self.viewContext else {
             return []
         }
 
-        let comicsManagedObjects = self.fetchComicRating(withPredicate: nil, managedContext: managedContext)
-        let comics = self.toComics(managedObjects: comicsManagedObjects)
-        return comics
-    }
-
-    private func fetchComicRating(withPredicate predicate: NSPredicate?, managedContext: NSManagedObjectContext) -> [NSManagedObject] {
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
-        fetchRequest.predicate = predicate
-        var result: [NSManagedObject] = []
+        var comics: [ComicRating] = []
         do {
-            result = try managedContext.fetch(fetchRequest)
+            let comicsManagedObjects = try managedContext.fetch(fetchRequest)
+            comics = self.toComics(managedObjects: comicsManagedObjects)
         } catch let error as NSError {
             print("Could not load comics: \(error.localizedDescription)")
         }
 
-        return result
+        return comics
     }
 
     private func toComics(managedObjects: [NSManagedObject]) -> [ComicRating] {
