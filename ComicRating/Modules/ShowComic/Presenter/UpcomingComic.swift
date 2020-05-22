82ba157a55525ff1c20c2a5f5e6b414437e5052f
Adriana Pineda <apineda@twilio.com>
2020-05-22 08:11:15 +0300

    Add file to put UpcomingComic only

--- ComicRating/Modules/ShowComic/Presenter/UpcomingComic.swift
+++ ComicRating/Modules/ShowComic/Presenter/UpcomingComic.swift
@@ -1,17 +1,18 @@
 //
-//  Comic.swift
+//  UpcomingComic.swift
 //  ComicRating
 //
-//  Created by Adriana Pineda on 31/03/2020.
+//  Created by Adriana Pineda on 22/05/2020.
 //  Copyright © 2020 Adriana Pineda. All rights reserved.
 //
+import Foundation
 
-struct Comic {
+struct UpcomingComic {
     var number: Int
     var month: Int
     var year: Int
     var day: Int
     var title: String
     var safeTitle: String
-    var img: String
+    var img: Data
 }
