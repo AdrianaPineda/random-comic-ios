579dd9076cf00f98ae3dabb87f658e9213f8b3f9
Adriana Pineda <apineda@twilio.com>
2020-07-16 21:42:24 +0300

    Put back method in Comic+Equatable class

--- ComicRatingTests/Entities/Comic+Equatable.swift
+++ ComicRatingTests/Entities/Comic+Equatable.swift
@@ -13,4 +13,10 @@ extension Comic: Equatable {
     public static func == (lhs: Comic, rhs: Comic) -> Bool {
         lhs.id == rhs.id && lhs.title == rhs.title && lhs.date == rhs.date && lhs.img == rhs.img && lhs.rating == rhs.rating
     }
+
+    func equal(to value: Comic) -> ParameterMatcher<Comic> {
+        return ParameterMatcher { comparedValue in
+            value == comparedValue
+        }
+    }
 }
