c90ef32638f81f5b4d54efc26102912fea42de07
Adriana Pineda <apineda@twilio.com>
2020-08-28 20:57:11 +0300

    Update star view

---
+++ ComicRating/App/CustomViews/StarView.swift
@@ -0,0 +1,29 @@
+//
+//  StarView.swift
+//  ComicRating
+//
+//  Created by Adriana Pineda on 28/08/2020.
+//  Copyright © 2020 Adriana Pineda. All rights reserved.
+//
+import UIKit
+
+class StarView: UIView {
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        let center = CGPoint(x: rect.width / 2.0, y: rect.height / 2.0)
+        let xCenter: CGFloat = center.x
+        let yCenter: CGFloat = center.y
+        let width = rect.width
+        let radius = width / 2.0
+        let flip: CGFloat = -1.0 // use this to flip the figure 1.0 or -1.0
+        let polySide = CGFloat(5)
+        let theta = 2.0 * Double.pi * Double(2.0 / polySide)
+        path.move(to: CGPoint(x: xCenter, y: radius * flip + yCenter))
+        for index in 1 ..< Int(polySide) {
+            let xValue: CGFloat = radius * CGFloat(sin(Double(index) * theta))
+            let yValue: CGFloat = radius * CGFloat(cos(Double(index) * theta))
+            path.addLine(to: CGPoint(x: xValue + xCenter, y: yValue * flip + yCenter))
+        }
+        path.close()
+    }
+}
--- ComicRating/App/CustomViews/UIView+Extensions.swift
+++ ComicRating/App/CustomViews/UIView+Extensions.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
+extension UIView {
     func configureUI() {
         backgroundColor = UIConfiguration.sharedInstance.getBackgroundColor()
     }
