ff8ba141f9a501afe73bf3acfbc3f7692dc9de89
Adriana Pineda <apineda@twilio.com>
2020-08-29 11:16:01 +0300

    Reorganize files

--- ComicRating/App/CustomViews/RatingView/StarView.swift
+++ ComicRating/App/CustomViews/RatingView/StarView.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class StarView: UIView {
+    private let pointOnStar = 5
     var fillColor: UIColor? { didSet { setNeedsDisplay() } }
 
     override func draw(_ rect: CGRect) {
@@ -19,14 +20,13 @@ class StarView: UIView {
         let yCenter: CGFloat = center.y
         let width = rect.width
         let radius = width / 2.0
-        let flip: CGFloat = -1.0 // use this to flip the figure 1.0 or -1.0
-        let polySide = CGFloat(5)
+        let polySide = CGFloat(pointOnStar)
         let theta = 2.0 * Double.pi * Double(2.0 / polySide)
-        path.move(to: CGPoint(x: xCenter, y: radius * flip + yCenter))
+        path.move(to: CGPoint(x: xCenter, y: -1 * radius + yCenter))
         for index in 1 ..< Int(polySide) {
             let xValue: CGFloat = radius * CGFloat(sin(Double(index) * theta))
             let yValue: CGFloat = radius * CGFloat(cos(Double(index) * theta))
-            path.addLine(to: CGPoint(x: xValue + xCenter, y: yValue * flip + yCenter))
+            path.addLine(to: CGPoint(x: xValue + xCenter, y: -1 * yValue + yCenter))
         }
         path.close()
         path.fill()
